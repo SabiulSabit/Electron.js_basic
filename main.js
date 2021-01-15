a3ac4e65fc7766ba7d34d67cd4656a27e3393098
SabitTheBot <iamsabit99@gmail.com>
2021-01-15 17:32:13 +0600

    window hide unitil contetn loading

--- main.js
+++ main.js
@@ -10,14 +10,16 @@ function createWindow () {
 
   mainWindow = new BrowserWindow({
     width: 1000, height: 800,
-    webPreferences: { nodeIntegration: true }
+    webPreferences: { nodeIntegration: true },
+    show: false
   })
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile('index.html')
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools()
 
+  mainWindow.once('ready-to-show',mainWindow.show)
   // Listen for window being closed
   mainWindow.on('closed',  () => {
     mainWindow = null
